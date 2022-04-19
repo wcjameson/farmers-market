77da16cbc6cab6f4e1ec4d639fa4513b4d449404
wcjameson <williamjameson90@gmail.com>
2022-04-19 10:45:57 -0700

    add initial porject setup
    Co-authored-by: Matthew Eilar <meilar@users.noreply.github.com>

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Header";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+
+  return (
+    <React.Fragment>
+      <div class="container">
+        <Header />
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Header() {
+  
+  return (
+    <React.Fragment>
+      <div class="jumbotron">
+        <h1 class="display-4">Avery's Organics</h1>
+        <p class="text-lead">Bringing organic produce from Northwest Oregon to market since 1996. Avery's. It's organic.</p>
+      </div>
+      
+    </React.Fragment>
+  );
+}
+
+export default Header;
